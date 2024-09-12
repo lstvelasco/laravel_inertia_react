1ad1d4c22a4774900088368e209e301a7292cfea
Luisito Velasco <lstvelasco@gmail.com>
2024-09-12 22:36:40 +0800

    Learn preserve scroll

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,14 @@
-import Layout from "../Layouts/Layout";
+import { Link } from "@inertiajs/react";
+// import Layout from "../Layouts/Layout";
 
 const Home = () => {
     return (
         <>
             <h1 className="title">Hello user</h1>
+
+            <Link preserveScroll className="block title mt-[1000px]" href="/">
+                {new Date().toLocaleTimeString()}
+            </Link>
         </>
     );
 };
